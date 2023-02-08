451379e8bbb5ac9c919bc25c1583b590dff36060
zarni-ein <filippov@mail.ch>
2023-02-08 13:31:09 +0300

    v1.1e: UI polish

--- js/scale.js
+++ js/scale.js
@@ -1,10 +1,15 @@
 function trickyScale() {
     // let rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
     let container = document.getElementsByClassName("custom-checkbox")[0];
-    console.log(container.offsetWidth);
+    // console.log(container.offsetWidth);
     let scale = container.offsetWidth / 14;
     document.documentElement.style.setProperty("--trickyScale", scale);
 };
 
-window.onstart = trickyScale();
+function onStart() {
+    trickyScale();
+    document.getElementsByName("password-settings")[0].reset();
+}
+
+window.onstart = onStart();
 window.onresize = trickyScale;
